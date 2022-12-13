af25ce98ba672c932b26556deda6d073dbcb7e5f
simires <costasimires@gmail.com>
2022-12-13 01:20:17 -0300

    add comentários das funções da heap, add main_heap.c e descrição na readme

--- TR5_535741.c
+++ TR5_535741.c
@@ -2,70 +2,71 @@
 #include <stdlib.h>
 #include "heap.h"
 
-HEAP * HEAP_create(int n, COMP * compara) {
-    HEAP * novo = malloc(sizeof(HEAP)); // ponteiro para o conjunto heap
-    novo -> N = n; // tamanho do vetor
-    novo -> P = 0; // num de elementos do vetor
-    novo -> elems = malloc(sizeof(void * ) * n);
-    for (int r = 0; r < n; r++) { // r é contador e index (posição do vetor) // a função for percorre o vetor
-        novo -> elems[r] = NULL; // novo -> elems vetor de ponteiros 
+HEAP * HEAP_create(int n, COMP * compara) { //criar heap; recebe seu tamanho e o parametro de comparacao entre os elementos
+    HEAP * novo = malloc(sizeof(HEAP)); //ponteiro para o conjunto heap
+    novo -> N = n; //tamanho da heap
+    novo -> P = 0; //numero de elementos da heap
+    novo -> elems = malloc(sizeof(void * ) * n); //alocar espaco para os elementos da heap
+    for (int r = 0; r < n; r++) {
+        novo -> elems[r] = NULL; //retirar lixo de memoria da heap
     }
-    novo -> comparador = compara; // definindo a função comparador do vetor
-    return novo; // retornando o conjunto de elementos
+    novo -> comparador = compara; //definir a funcao comparador do vetor
+    return novo; // retornar a heap criada
 }
 
-void HEAP_add(HEAP * heap, void * newelem) {
-    if (heap -> P < heap -> N) {
-        if (heap -> P == 0) { // checando se o número de elemtos é igual a zero
-            heap -> elems[0] = newelem;
-            heap -> P++; // adiciona mais um ao número de elementos ao vetor 
-        } else {
-            int pos = heap -> P;
-            int pai = (pos - 1) / 2; // indice do pai
-            heap -> elems[pos] = newelem; // adicionando na última posição do vetor
-            while (heap -> comparador(heap -> elems[pos], heap -> elems[pai]) == 1) { // enquanto o elemento na posição é menor que o pai dele 
-                void * aux = heap -> elems[pai]; // acessando o vetor no index pai e salvando em aux
-                heap -> elems[pai] = heap -> elems[pos]; // trocando as posições
-                heap -> elems[pos] = aux; // trocando pos para aux que era pai 
-                pos = pai; // pos se torna pai
-                pai = (pos - 1) / 2; // novo pai sendo calculado
+void HEAP_add(HEAP * heap, void * newelem) { //adicionar elementos; recebe a heap e o novo elemento
+    if (heap -> P < heap -> N) { //heap nao esta cheia
+        if (heap -> P == 0) { //heap esta vazia
+            heap -> elems[0] = newelem; //inserir elem na primeira posicao
+            heap -> P++; //aumentar tam da heap sempre que um elem for adicionado
+        } else { //heap nao esta vazia
+            int pos = heap -> P; //iterador
+            int pai = (pos - 1) / 2; //indice do pai
+            heap -> elems[pos] = newelem; //adiciona na ultima posicao do vetor
+            while (heap -> comparador(heap -> elems[pos], heap -> elems[pai]) == 1) { 
+                //enquanto o elemento na posicao eh menor que o pai dele 
+                void * aux = heap -> elems[pai]; //aux guarda o valor do pai
+                heap -> elems[pai] = heap -> elems[pos]; //pai recebe o antigo filho
+                heap -> elems[pos] = aux; //o antigo filho recebe o antigo pai
+                pos = pai; //filho antigo se torna pai
+                pai = (pos - 1) / 2; //novo indice do pai eh calculado
                 if (pai < 0) break;
             }
-            heap->P++;
+            heap->P++; //aumentar o tam da heap sempre que um elem for adicionado
         }
     }
 }
 
-void* HEAP_remove(HEAP* heap){
-    void* aux = heap->elems[0];
-    heap->elems[0] = heap->elems[heap->P - 1];
-    heap->elems[heap->P - 1] = NULL;
-    heap->P--;
+void* HEAP_remove(HEAP* heap){ //remover; recebe a heap e remove o seu primeiro elemento
+    void* aux = heap->elems[0]; //guarda elem a ser removido
+    heap->elems[0] = heap->elems[heap->P - 1]; //a posicao desocupada recebe o ultimo elem
+    heap->elems[heap->P - 1] = NULL; //liberar a ultima posicao
+    heap->P--; //reduzir o tamanho da heap
 
-    int pos = 0;
-    int f1, f2;
+    int pos = 0; //iterador
+    int f1, f2; //filho 1 e filho 2
     int menor;
-    while(2*pos+1 < heap->P){
-        f1 = 2*pos+1;
-        f2 = 2*pos+2;
-        if(heap->elems[f1] != NULL && heap->elems[f2] != NULL){
-            menor = heap->elems[f1] < heap->elems[f2] ? f1 : f2;
-            if(heap->elems[menor] < heap->elems[pos]){
-                void* aux = heap->elems[menor];
-                heap->elems[menor] = heap->elems[pos];
-                heap->elems[pos] = aux;
-                pos = menor;
+    while(2*pos+1 < heap->P){ //percorrer toda a heap
+        f1 = 2*pos+1; //indice filho 1
+        f2 = 2*pos+2; //indice filho 2
+        if(heap->elems[f1] != NULL && heap->elems[f2] != NULL){ //se o pai tem dois filhos
+            menor = heap->elems[f1] < heap->elems[f2] ? f1 : f2; //menor vai guardar o menor dos valores entre os filhos
+            if(heap->elems[menor] < heap->elems[pos]){ //se o filho for menor que o pai troca os valores
+                void* aux = heap->elems[menor]; //aux guarda o menor filho
+                heap->elems[menor] = heap->elems[pos]; //o antigo filho menor recebe o pai
+                heap->elems[pos] = aux; //o antigo pai recebe o menor filho
+                pos = menor; //atualizar o indice do pai
             } else break;
-        } else{
-            if(heap->elems[f1] != NULL){
-                if(heap->elems[f1] < heap->elems[pos]){
+        } else{ //se o pai nao tem dois filhos
+            if(heap->elems[f1] != NULL){ //se o pai tem somente o f1
+                if(heap->elems[f1] < heap->elems[pos]){ //se o filho for menor que o pai, troca os valores entre eles
                     void* aux = heap->elems[f1];
                     heap->elems[f1] = heap->elems[pos];
                     heap->elems[pos] = aux;
                     pos = f1;
                 } else break;
-            } else{
-                if(heap->elems[f2] < heap->elems[pos]){
+            } else{ //se o pai tem soente o filho 2
+                if(heap->elems[f2] < heap->elems[pos]){ //se o filho 2 for menor que o pai, troca os valores
                     void* aux = heap->elems[f2];
                     heap->elems[f2] = heap->elems[pos];
                     heap->elems[pos] = aux;
@@ -75,5 +76,5 @@ void* HEAP_remove(HEAP* heap){
         }
     }
 
-    return aux;
+    return aux; //retornar o valor removido
 }
